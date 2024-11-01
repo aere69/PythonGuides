dfd032ed49241b62ef9db8bddd85f14a5053b3c4
Andres E. Rojas E. <aere69@gmail.com>
2024-11-01 17:31:32 +0000

    Higher Lower Game

---
+++ Projects/HigherLower/HigherLowe.py
@@ -0,0 +1,48 @@
+from art import logo,vs
+from game_data import data
+import random
+
+def select_celebrity():
+    return random.choice(data)
+
+def check_answer(celeb_A, celeb_B, guess):
+    if celeb_A["follower_count"] > celeb_B["follower_count"]:
+        return "A" == guess
+    else:
+        return "B" == guess
+
+def game():
+    game_over = False
+
+    celebrity1 = {}
+    celebrity2 = {}
+
+    score = 0
+
+    celebrity1 = select_celebrity()
+
+    while not game_over:
+        print(logo)
+        
+        if score != 0:
+            print(f"Correct!!!. Current Score : {score}\n")
+
+        print(f"Compare A: {celebrity1["name"]}, a {celebrity1["description"]}, from {celebrity1["country"]}")
+        
+        print(vs)
+        
+        celebrity2 = select_celebrity()    
+        print(f"Against B: {celebrity2["name"]}, a {celebrity2["description"]}, from {celebrity2["country"]}")
+
+        answer = input("\nWho has more followers, \'A\' or \'B\'? ").upper()
+
+        if check_answer(celebrity1, celebrity2, answer):
+            score += 1
+            celebrity1 = celebrity2
+        else:
+            game_over = True
+
+    print(logo)
+    print(f"You Lost. Your final Score : {score}\n")
+
+game()
---
+++ Projects/HigherLower/description.md
@@ -0,0 +1,13 @@
+# Higher Lower Game (Beginner)
+
+Build a game that asks the user to guess who has more followers on Instagram.
+
+
+## Celebrity Information
+
+Celebrity Information is available on `game_data.py`
+
++ name
++ follower count (in Millions)
++ description
++ country
