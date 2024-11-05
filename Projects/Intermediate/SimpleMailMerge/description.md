ca6cd3f7ef07a5513dea0ac14f79b270b37a2da0
Andres E. Rojas E. <aere69@gmail.com>
2024-11-05 18:55:02 +0000

    Simple Mail Merge

---
+++ Projects/Intermediate/SimpleMailMerge/description.md
@@ -0,0 +1,9 @@
+# Simple Mail Merge
+
+Create a program that:
+
+1. Takes a list of names from a file (invited_names.txt)
+
+2. Takes an email template (starting_letter.txt)
+
+3. For each name on the list replace the place holder on the template with the name and save to a new file ([name]_invitation_letter.txt)
---
+++ Projects/Intermediate/SimpleMailMerge/main.py
@@ -0,0 +1,25 @@
+PATH_TO_PROJECT = "./Projects/Intermediate/SimpleMailMerge"
+filename = PATH_TO_PROJECT + "/Input/Names/invited_names.txt"
+
+def load_doc_template(file_name):
+    with open(file_name) as file:
+        doc = file.read()
+    return doc
+
+def create_invitation(to, document):
+    doc = document.replace("[name]", to)
+    to = to.replace(" ", "_")
+    filename = PATH_TO_PROJECT+"/Output/ReadyToSend/"+to+"_invitation_letter.txt"
+    with open(filename, "w") as file:
+        file.write(doc)
+
+try:
+    f = open(filename, "r")
+    template = load_doc_template(PATH_TO_PROJECT+"/Input/Letters/starting_letter.txt")
+
+    for invited_name in f:
+        invited_name = invited_name.strip("\n")
+        create_invitation(invited_name,template)
+except FileNotFoundError:
+    print(f"Error - File not found.")
+    exit()
