2514a19ab0985256364c8a832710b7c5fb186b6e
Andres E. Rojas E. <aere69@gmail.com>
2024-11-06 19:10:57 +0000

    Apply List Comprehension

--- Projects/Intermediate/US_States_Game/main.py
+++ Projects/Intermediate/US_States_Game/main.py
@@ -15,10 +15,14 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # Convert to List Comprehension
+        # missing_states = []
+        # for state in all_states:
+        #    if state not in guessed_states:
+        #        missing_states.append(state)
+
+        # Using List Comprehension
+        missing_states = [state for state in all_states if state not in guessed_states]        
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
