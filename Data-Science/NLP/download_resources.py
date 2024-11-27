d765e66b371381c44c9916c9c844fde06bc05daa
Andres E. Rojas E. <aere69@gmail.com>
2024-11-27 15:26:23 +0000

    Data Science Topic restructure

---
+++ Data-Science/NLP/download_resources.py
@@ -0,0 +1,10 @@
+import nltk
+
+# Download essential datasets and models
+nltk.download('punkt')  # Tokenizers for sentence and word tokenization
+nltk.download('stopwords')  # List of common stop words
+nltk.download('wordnet')  # WordNet lexical database for lemmatization
+nltk.download('averaged_perceptron_tagger_eng')  # Part-of-speech tagger
+nltk.download('maxent_ne_chunker_tab')  # Named Entity Recognition model
+nltk.download('words')  # Word corpus for NER
+nltk.download('punkt_tab')
